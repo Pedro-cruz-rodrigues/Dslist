f751e98e618b69dcc6f815a9a805885fa5a706e6
Pedro <jp.cruz.jp@outlook.com>
2025-01-25 15:32:30 -0300

    alteracoes na sequencia de variaveis na classe Game.java

--- src/main/java/com/Devsuperior/dslist/entities/Game.java
+++ src/main/java/com/Devsuperior/dslist/entities/Game.java
@@ -21,13 +21,13 @@ public class Game {
 		@GeneratedValue(strategy = GenerationType.IDENTITY)		
 		private Long id;
 		private String title;
-		private Double score;
+		
 		
 		@Column(name = "game_year")
 		private Integer year;
 		private String genre;
 		private String platforms;
-		
+		private Double score;
 		private String imgUrl;
 		
 		@Column(columnDefinition = "TEXT")
@@ -39,7 +39,8 @@ public class Game {
 		public Game() {
 		}
 
-		public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imageUrl,
+	
+		public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 				String shortDescription, String longDescription) {
 			this.id = id;
 			this.title = title;
@@ -47,10 +48,11 @@ public class Game {
 			this.genre = genre;
 			this.platforms = platforms;
 			this.score = score;
-			this.imgUrl = imageUrl;
+			this.imgUrl = imgUrl;
 			this.shortDescription = shortDescription;
 			this.longDescription = longDescription;
 		}
+
 
 		public Long getId() {
 			return id;
@@ -140,6 +142,8 @@ public class Game {
 			Game other = (Game) obj;
 			return Objects.equals(id, other.id);
 		}
+
+	
 		
 		
 		
